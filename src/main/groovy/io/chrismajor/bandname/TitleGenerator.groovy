2608741ba37393d43445dd2a34033f59eea662b6
Chris <theactionslacks@gmail.com>
2017-01-22 19:42:44 +0000

    added in commentary

--- src/main/groovy/io/chrismajor/bandname/TitleGenerator.groovy
+++ src/main/groovy/io/chrismajor/bandname/TitleGenerator.groovy
@@ -1,14 +1,19 @@
 package io.chrismajor.bandname
 
 class TitleGenerator {
+	// TODO: logging https://dzone.com/articles/log4j-groovy-way
+
+	/** XML parser */
 	XmlParser parser
 	
+	/** Constructor. Set up xml parser */
 	TitleGenerator() {
 		parser = new XmlParser()
 		parser.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false)
 		parser.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false)
 	}
 	
+	/** return a valid, nicely formatted title */
 	String getTitle() {
 		String title
 		boolean valid = false
@@ -22,20 +27,28 @@ class TitleGenerator {
 		return title
 	}
 	
+	/** call off to Wikipedia to grab a new title! */
 	private String grabTitleFromWeb() {
+		// get the raw HTML from a random article
 		def wikiTxt = new URL("https://en.wikipedia.org/wiki/Special:Random").getText()
+		
+		// parse the HTML
 		def wikiXml = parser.parseText(wikiTxt)
 		
+		// grab the title from the relevant HTML element
 		return wikiXml.depthFirst().find {
 			it.@id == "firstHeading"
 		}.text()
 	}
 	
+	/** filter a title */
 	private String filterTitle(String title) {
+		// remove the bracket suffix (if exists)
 		String filteredTitle = title.split("\\(")[0]
 		return filteredTitle
 	}
 	
+	/** validate a title */
 	private boolean isValid(String title) {
 		def words = title.split(" ").size()
 		def length = title.length()
