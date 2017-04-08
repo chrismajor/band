032bd9fadc47417b96d5826335fa67abf7e48014
Chris <theactionslacks@gmail.com>
2017-04-08 09:08:02 +0100

    added in basic config stuff for naming patterns

---
+++ config/NamingPatterns.groovy
@@ -0,0 +1,19 @@
+// package io.chrismajor.bandname.config
+
+/*
+Groovy script to act as configuration for band name patterns
+To be used in conjunction with groovy.util.ConfigSlurper
+ */
+namingPatterns = [
+    "The adjective nouns",
+    "Name and the nouns",
+    "Verbing name",
+    "Noun",
+    "The nouns",
+    "(3-7) nouns",
+    "Adjective adjective",
+    "Pronoun verbs",
+    "The full-name experience",
+    "The name verb experience",
+    "Adverb colour"
+]
--- src/main/groovy/io/chrismajor/bandname/Main.groovy
+++ src/main/groovy/io/chrismajor/bandname/Main.groovy
@@ -1,6 +1,11 @@
 package io.chrismajor.bandname
 
-import io.chrismajor.bandname.wiki.TitleGenerator
+//import io.chrismajor.bandname.wiki.TitleGenerator
+//
+//def generator = new TitleGenerator()
+//println generator.getTitle()
 
-def generator = new TitleGenerator()
-println generator.getTitle()
+def config = new ConfigSlurper().parse(new URL("config/NamingPatterns.groovyo"))
+config.namingPatterns.each {
+    println it
+}
