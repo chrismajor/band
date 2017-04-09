e857183e6f5eab61f60da3b52b2a3d4d9b5f98f7
Chris <theactionslacks@gmail.com>
2017-04-09 20:58:48 +0100

    basic algorithm fleshed out for replacing token placeholders in a band name pattern with a random value for that token. For some reason the text replacing isn't currently working...

--- config/NamingPatterns.groovy
+++ config/NamingPatterns.groovy
@@ -3,15 +3,16 @@ Groovy script to act as configuration for band name patterns
 To be used in conjunction with groovy.util.ConfigSlurper
  */
 namingPatterns = [
-    "The adjective nouns",
-    "Name and the nouns",
-    "Verbing name",
-    "Noun",
-    "The nouns",
-    "(3-7) nouns",
-    "Adjective adjective",
-    "Pronoun verbs",
-    "The full-name experience",
-    "The name verb experience",
-    "Adverb colour"
+    "The [adjective] [noun]s",
+    "[name] and the [noun]s",
+    "[name]'s [noun]",
+    "[verb]ing [name]",
+    "[noun]",
+    "The [noun]s",
+    "[3-7] [noun]s",
+    "[adjective] [adjective]",
+    "[pronoun] [verb]s",
+    "[adverb] [colour]",
+//    "The [full-name] experience",
+    "The [name] [verb] experience"
 ]
--- src/main/groovy/io/chrismajor/bandname/Main.groovy
+++ src/main/groovy/io/chrismajor/bandname/Main.groovy
@@ -1,5 +1,7 @@
 package io.chrismajor.bandname
 
+import io.chrismajor.bandname.tokens.TokenTypes
+
 //import io.chrismajor.bandname.wiki.TitleGenerator
 //
 //def generator = new TitleGenerator()
@@ -8,8 +10,29 @@ package io.chrismajor.bandname
 // TODO: use non-deprecated stuff here :/
 def config = new ConfigSlurper().parse(new File("config/NamingPatterns.groovy").toURL())
 
-config.namingPatterns.each { String pattern ->
-    pattern.toLowerCase().split("noun").each {
-        println "'$it'"
+// get a random pattern from the config file
+int patternCount = config.namingPatterns.size()
+println "patternCount :: $patternCount"
+
+int randomElement = new Random().nextInt(patternCount)
+println "randomElement :: $randomElement"
+
+String pattern = config.namingPatterns.get(randomElement)
+println "pattern :: $pattern"
+
+// for each of the token types, replace the token as necessary with a token value
+TokenTypes.getEnumConstants().each { token ->
+    def propertyToken = "[$token.name]"
+
+    println ""
+    println "propertyToken          :: $propertyToken"
+    println "pattern contains token :: " + pattern.contains(propertyToken)
+
+    if (pattern.contains(propertyToken)) {
+        // TODO: debug why this doesn't currently replace anything...
+        pattern.replaceAll(propertyToken, token.tokenFactory.getTokenValue().capitalize())
     }
 }
+
+println ""
+println "processed pattern :: $pattern"
