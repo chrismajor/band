fb6bbd4cd4791bd87d71063f8200d766e210ee30
Chris <theactionslacks@gmail.com>
2017-09-09 11:37:59 +0100

    repeated tokens get unique values

--- src/main/groovy/io/chrismajor/bandname/Main.groovy
+++ src/main/groovy/io/chrismajor/bandname/Main.groovy
@@ -2,10 +2,7 @@ package io.chrismajor.bandname
 
 import io.chrismajor.bandname.tokens.TokenTypes
 
-//import io.chrismajor.bandname.wiki.TitleGenerator
-//
-//def generator = new TitleGenerator()
-//println generator.getTitle()
+import java.util.regex.Pattern
 
 // TODO: use non-deprecated stuff here :/
 def config = new ConfigSlurper().parse(new File("config/NamingPatterns.groovy").toURL())
@@ -23,16 +20,15 @@ println "pattern :: $pattern"
 
 // for each of the token types, replace the token as necessary with a token value
 TokenTypes.getEnumConstants().each { token ->
-    def propertyToken = "[$token.name]"
+    final String propertyToken = "[$token.name]"
 
     println ""
     println "propertyToken          :: $propertyToken"
     println "pattern contains token :: ${pattern.contains(propertyToken)}"
 
-    if (pattern.contains(propertyToken)) {
+    while (pattern.contains(propertyToken)) {
         println "token replacement value :: ${token.tokenFactory.getTokenValue().capitalize()}"
-        // TODO: replace the first instance of the token only
-        pattern = pattern.replace(propertyToken, token.tokenFactory.getTokenValue().capitalize())
+        pattern = pattern.replaceFirst(Pattern.quote(propertyToken), token.tokenFactory.getTokenValue().capitalize())
     }
 }
 
